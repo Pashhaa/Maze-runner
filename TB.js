8c3d9ca545bfebf3fdeb46364a400a7d31276b94
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-01-25 01:23:35 +0300

    Update TB.js

--- TB.js
+++ TB.js
@@ -1,18 +1,18 @@
 
-let TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 
-let token = '---';
-let bot = new TelegramBot(token, {polling: true});
+const token = '---';
+const bot = new TelegramBot(token, {polling: true});
 
 
 
 
 bot.onText(/toroman (.+)/, function  (msg,b){
-  let userId = msg.from.id;
-  let a = b[1]
+  const tuserId = msg.from.id;
+  const a = b[1]
   let roman = '';
   if (a < 999999 && a > 0){
-  let numbers = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+  const numbers = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
   for (let key in numbers){
     while (a >= numbers[key]){
       roman += key;
@@ -28,10 +28,10 @@ bot.onText(/toroman (.+)/, function  (msg,b){
 
 
 bot.onText(/start/, function(msg){
-  let userId = msg.from.id;
+  const userId = msg.from.id;
   bot.sendMessage(userId, 'Привет! Чтобы увидеть полный список команд: /help ');
 });
 bot.onText(/help/, function(msg){
-  let userId = msg.from.id;
+  const userId = msg.from.id;
   bot.sendMessage(userId,    'Команда"toroman"("toroman АРАБСКОЕ_ЧИСЛО") - переводит арабские числа в римские(до 5000)')
 })
