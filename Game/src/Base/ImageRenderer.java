49129e9fa6fe8dec212669d179d3cdb44ca6f69a
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-02-06 14:39:39 +0300

    Update ImageRenderer.java

--- Game/src/Base/ImageRenderer.java
+++ Game/src/Base/ImageRenderer.java
@@ -1,12 +1,11 @@
 package Base;
 
+import Base.Objects.AbstractFigur;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
 
-/**
- * Created by Asus on 17.08.2020.
- */
 public class ImageRenderer extends DefaultTableCellRenderer {
 
     private JLabel lbl = new JLabel();
@@ -16,21 +15,12 @@ public class ImageRenderer extends DefaultTableCellRenderer {
                                                    boolean hasFocus, int row, int column) {
 
 
+
+        AbstractFigur currentObject = (AbstractFigur) value;
         lbl.setText(null);
 
-        if (value.equals("B")) {
-            lbl.setIcon(new ImageIcon(getClass().getResource("/images/wall.png")));
-        } else if (value.equals("P")) {
-            lbl.setIcon(new ImageIcon(getClass().getResource("/images/goldman_up.png")));
-        } else if (value.equals("M")) {
-            lbl.setIcon(new ImageIcon(getClass().getResource("/images/monster_up.jpg")));
-        } else if (value.equals("E")) {
-            lbl.setIcon(new ImageIcon(getClass().getResource("/images/exit.gif")));
-        } else if (value.equals("G")) {
-            lbl.setIcon(null);
-        } else if (value.equals("GG")) {
-            lbl.setIcon(new ImageIcon(getClass().getResource("/images/gold.png")));
-        }
+        lbl.setIcon(currentObject.getImage());
+
 
 
         return lbl;
