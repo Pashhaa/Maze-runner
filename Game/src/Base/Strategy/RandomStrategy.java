d765e5a364d86f5081cce64a496cc30fb8c24468
Pashhaa <71789749+Pashhaa@users.noreply.github.com>
2021-03-27 15:44:38 +0300

    MethodAdding

--- Game/src/Base/Strategy/RandomStrategy.java
+++ Game/src/Base/Strategy/RandomStrategy.java
@@ -4,10 +4,15 @@ import Base.Collection.GameCollection;
 import Base.Objects.Abstracts.AbstractMovingFigur;
 import Base.Objects.Enums.Direction;
 
+import java.util.Random;
+
 public class RandomStrategy implements MovingStrategy{
 
     @Override
     public Direction getDirection(GameCollection collection, AbstractMovingFigur movingFigur) {
-        return null;
+        Direction[] directions = Direction.values();
+        Random random = new Random();
+        int index = random.nextInt(4);
+        return directions[index];
     }
 }
